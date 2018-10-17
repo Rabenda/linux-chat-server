2ec7500ad5ffe0ec576d231ee9f6e688ec40ec8d
Rabenda <rabenda.cn@gmail.com>
2018-10-17 22:47:55 +0800

    Init project and configure conan

---
+++ README.md
@@ -0,0 +1,14 @@
+# linux-chat-server
+
+### dependence
+```bash
+conan install . -s build_type=Release
+```
+
+### Compile
+```
+mkdir build
+cd build
+cmake ..
+make -j4
+```
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/asio.hpp>
+using namespace std;
+
+int main()
+{
+    boost::asio::io_service service;
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
